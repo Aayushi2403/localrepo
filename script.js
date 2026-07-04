15bde5c7d970d77f475ed06507c84ce363e0adcf
Aayushi Gupta <ayugp1312@gmail.com>
2026-07-05 01:16:07 +0530

    Add calculate function for basic arithmetic operations

---
+++ script.js
@@ -0,0 +1,19 @@
+function calculate(a, b, operator) {
+    switch (operator) {
+        case "+":
+            return a + b;
+        case "-":
+            return a - b;
+        case "*":
+            return a * b;
+        case "/":
+            return b !== 0 ? a / b : "Cannot divide by zero";
+        default:
+            return "Invalid operator";
+    }
+}
+
+console.log(calculate(10, 5, "+"));
+console.log(calculate(10, 5, "-"));
+console.log(calculate(10, 5, "*"));
+console.log(calculate(10, 5, "/"));
